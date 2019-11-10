ce2fb59711ef4ba7f264691e36938a19eb6d7cdd
Henrique Silva <henrique.p.silva@outlook.com>
2019-11-10 17:41:07 -0300

    added julia sources

---
+++ src/MathSolver.jl
@@ -0,0 +1,31 @@
+module MathSolver
+
+using JuMP
+using GLPK
+
+export solve
+
+# just an example
+# i still need to look into the parsing of a file and the passage of parameters
+# use ritt's example solutions!
+function solve()
+    m = Model(with_optimizer(GLPK.Optimizer))
+
+    @variable(m, x1 >= 0)
+    @variable(m, x2 >= 0);
+
+    @objective(m,Max,10*x1+20*x2)
+
+    @constraints(m, begin
+                 0.1*x1+0.1*x2 <= 200
+                 0.125*x2 <= 800
+                 2*x1+3*x2 <= 5*40*60
+                 end)
+
+    optimize!(m)
+
+    println("A solucao otima e vender $(value(x1)) paes e $(value(x2)) baurus
+    completos com um lucro total de $(objective_value(m)).")
+end
+
+end # module
---
+++ src/SimulatedAnnealing.jl
@@ -0,0 +1,3 @@
+module SimulatedAnnealing
+
+end # module
